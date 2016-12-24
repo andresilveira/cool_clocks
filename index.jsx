d081765b8ee3f1c487b4f4eab960ff18ea28ba79
André Herculano <andresilveirah@gmail.com>
2016-12-24 14:35:12 +0100

    initial setup

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ItWorks = (props) => (
+  <h1>It works</h1>
+);
+
+ReactDOM.render(
+  <ItWorks />,
+  document.getElementById("app")
+);
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./index.jsx",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env']
+        }
+      }
+    ]
+  }
+};
